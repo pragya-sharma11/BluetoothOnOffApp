4745646fe154562e4f6ac26ced685d6a72212af0
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-04 20:59:07 +0530

    find buttons by id and get service for bluetooth.

--- app/src/main/java/com/example/bluetoothonoffapp/MainActivity.java
+++ app/src/main/java/com/example/bluetoothonoffapp/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.bluetoothonoffapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1, b2;
+    BluetoothAdapter b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.button);
+        b2 = findViewById(R.id.button2);
+        b = BluetoothAdapter.getDefaultAdapter();
+
     }
 }
