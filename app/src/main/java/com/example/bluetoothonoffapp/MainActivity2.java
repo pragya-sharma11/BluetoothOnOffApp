ab2a8bec8aa807ce077781d2a52bf0be024eedb4
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-04 22:39:55 +0530

    make functionality on clicking of button 4 to go to next page.

--- app/src/main/java/com/example/bluetoothonoffapp/MainActivity2.java
+++ app/src/main/java/com/example/bluetoothonoffapp/MainActivity2.java
@@ -3,11 +3,14 @@ package com.example.bluetoothonoffapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity2 extends AppCompatActivity {
+    Button btn;
     ImageButton i;
     BluetoothAdapter b;
     private  boolean bt = false;
@@ -17,6 +20,7 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         i = findViewById(R.id.imageButton);
         b = BluetoothAdapter.getDefaultAdapter();
+        btn = findViewById(R.id.button4);
         i.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,6 +32,14 @@ public class MainActivity2 extends AppCompatActivity {
                     i.setImageResource(R.drawable.c);
                     b.disable();
                 }
+            }
+        });
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
