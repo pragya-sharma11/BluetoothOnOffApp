6b904fd8286bf10de69c66f0bcea452670d67e06
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-04 21:17:23 +0530

    on off bluetooth button with toggle button.

--- app/src/main/java/com/example/bluetoothonoffapp/MainActivity3.java
+++ app/src/main/java/com/example/bluetoothonoffapp/MainActivity3.java
@@ -2,13 +2,30 @@ package com.example.bluetoothonoffapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class MainActivity3 extends AppCompatActivity {
-
+    ToggleButton t;
+    BluetoothAdapter b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        t = findViewById(R.id.toggleButton);
+        b = BluetoothAdapter.getDefaultAdapter();
+        t.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked==false){
+                    b.enable();
+                }
+                else{
+                    b.disable();
+                }
+            }
+        });
     }
 }
