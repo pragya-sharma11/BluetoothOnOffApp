6c6e4ad949714d0ae41dc3ba0f05d01728ccd174
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-04 21:02:07 +0530

    declare variables

--- app/src/main/java/com/example/bluetoothonoffapp/MainActivity2.java
+++ app/src/main/java/com/example/bluetoothonoffapp/MainActivity2.java
@@ -2,10 +2,14 @@ package com.example.bluetoothonoffapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    ImageButton i;
+    BluetoothAdapter b;
+    private  boolean bt = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
