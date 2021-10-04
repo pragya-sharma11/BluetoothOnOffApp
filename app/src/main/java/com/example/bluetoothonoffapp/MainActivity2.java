57a7ff4547c771f6da6328f6e5f0e09e5eac5c90
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-04 21:11:44 +0530

    on off functionality for bluetooth

--- app/src/main/java/com/example/bluetoothonoffapp/MainActivity2.java
+++ app/src/main/java/com/example/bluetoothonoffapp/MainActivity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -16,6 +17,18 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         i = findViewById(R.id.imageButton);
         b = BluetoothAdapter.getDefaultAdapter();
-
+        i.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!bt){
+                    b.enable();
+                    i.setImageResource(R.drawable.c);
+                }
+                else {
+                    i.setImageResource(R.drawable.c);
+                    b.disable();
+                }
+            }
+        });
     }
 }
