70ab8581e69385647561490d8c6830b1d909070e
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-04 22:35:54 +0530

    add functionality to go to next page.

--- app/src/main/java/com/example/bluetoothonoffapp/MainActivity.java
+++ app/src/main/java/com/example/bluetoothonoffapp/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.bluetoothonoffapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1, b2;
+    Button b1, b2, b3;
     BluetoothAdapter b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 b.disable();
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+                finish();
             }
         });
     }
